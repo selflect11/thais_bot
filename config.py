f842039767a6176d85c77ff20917b695b43a034a
selflect11 <pedrovolpinacif@gmail.com>
2019-04-07 18:48:08 -0400

    Added reorganize fields functionality

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+DEBUG = False
 FILE_INPUT_PATH = './files/input_test_copy.docx'
 MAIN_HEADER = r'Código:'
 BANCA_HEADER = ORGAO_HEADER = r'Banca: (?P<banca>.+?) \((?P<orgao>.+?)\)'
 ANO_HEADER = r'Ano: (?P<ano>\d{4})'
-OUT_MAIN_HEADER = '({orgao} - {banca} - {ano})'
 ESTADO_HEADER = 'Estado:'
 DISCIPLINA_HEADER = 'Disciplina:'
 NIVEL_HEADER = 'Nível: (.+?)'
 DICA_HEADER = 'Dica do autor:'
-DELETE_HEADERS = [MAIN_HEADER, BANCA_HEADER, ANO_HEADER, ESTADO_HEADER, DISCIPLINA_HEADER, NIVEL_HEADER, DICA_HEADER]
+QUESTOES_HEADER = 'Alternativa A:'
+REFERENCIAS_HEADER = 'Referências bibliográficas:'
+DELETE_HEADERS = [MAIN_HEADER, BANCA_HEADER, ANO_HEADER, ESTADO_HEADER, DISCIPLINA_HEADER, NIVEL_HEADER]
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import re
 import code
+from typing import List
 from config import *
 from itertools import tee
-from docx import Document as doc
+from docx import Document
 
 
 def pairwise(iterable):
@@ -11,77 +12,132 @@ def pairwise(iterable):
     next(b, None)
     return zip(a, b)
 
-def get_header_indexes(paragraph_list):
-    cod_pat = re.compile(MAIN_HEADER)
-    header_indexes = []
-    for i, p in enumerate(paragraph_list):
-        if re.search(cod_pat, p.text):
-            header_indexes.append(i)
-    return header_indexes
+def get_page_blocks(paragraph_list):
+    # receives paragraph objects rather than text
 
-def get_pages(paragraph_list):
+    def get_page_indexes(paragraph_list):
+        header_pat = re.compile(MAIN_HEADER)
+        header_indexes = []
+        for i, paragraph in enumerate(paragraph_list):
+            if re.search(header_pat, paragraph.text):
+                header_indexes.append(i)
+        return header_indexes
+
     pages = []
-    header_indexes = get_header_indexes(d.paragraphs)
+    header_indexes = get_page_indexes(paragraph_list)
     for i_0, i_1 in pairwise(header_indexes):
         pages.append(paragraph_list[i_0:i_1])
-    pages.append(paragraph_list[header_indexes[-1]:-1])
+    # last page doesnt have last+1 corresponding header index, so we do this
+    last_i = header_indexes[-1]
+    pages.append(paragraph_list[last_i:-1])
     return pages
 
-def format_header(page):
-    # orgao nao pegou
+def generate_header(page):
     ano, banca, orgao = '', '', ''
-    for line in page:
-        search_banca = re.search(BANCA_HEADER, line.text)
-        search_orgao = re.search(ORGAO_HEADER, line.text)
-        search_ano = re.search(ANO_HEADER, line.text)
-        if search_banca or search_orgao:
+    for paragraph in page:
+        search_banca = re.search(BANCA_HEADER, paragraph.text)
+        search_orgao = re.search(ORGAO_HEADER, paragraph.text)
+        search_ano = re.search(ANO_HEADER, paragraph.text)
+        if search_banca:
             banca = search_banca.group('banca')
-            orgao = search_orgao.group('orgao')
+            # orgao not always present. When it is, it's on the same paragraph as banca
+            if search_orgao:
+                orgao = search_orgao.group('orgao')
         elif search_ano:
             ano = search_ano.group('ano')
         elif ano and banca and orgao:
             break
-    return OUT_MAIN_HEADER.format(ano=ano, banca=banca, orgao=orgao)
+    if orgao:
+        return '({orgao} - {banca} - {ano}) '.format(ano=ano, banca=banca, orgao=orgao)
+    else:
+        return '({banca} - {ano}) '.format(ano=ano, banca=banca)
 
 def field_filter(page):
     # returns only fields that are supposed to be inaltered
+    empty_pat = re.compile(r'(?:\s+)|(?:^$)')
+    between_disc_and_nivel = False
     filtered_page = []
-    for line in page:
-        delete_paragraphs = [re.search(pat, line.text) for pat in DELETE_HEADERS]
-        if not any(delete_paragraphs):
-            filtered_page.append(line.text)
+
+    for paragraph in page:
+        is_empty = re.match(empty_pat, paragraph.text)
+        delete_paragraphs = [re.search(pat, paragraph.text) for pat in DELETE_HEADERS]
+        if re.search(DISCIPLINA_HEADER, paragraph.text):
+            between_disc_and_nivel = True
+        elif re.search(NIVEL_HEADER, paragraph.text):
+            between_disc_and_nivel = False
+        elif not any(delete_paragraphs) and not is_empty and not between_disc_and_nivel:
+            filtered_page.append(paragraph)
     return filtered_page
 
-def output_doc(page_list):
+def field_reorganize(page):
+    # Move 'Dica do autor' to before 'Referencias bibliograficas'
+    # 'Dica do autor' ends when 'Alternativa A:' starts
+    organized_paragraphs = []
+
+    def get_dicas(page):
+        is_dicas_block = False
+        dicas = []
+        for paragraph in page:
+            if re.search(DICA_HEADER, paragraph.text):
+                is_dicas_block = True
+                dicas.append(paragraph)
+                break
+            elif re.search(QUESTOES_HEADER, paragraph.text):
+                # for readability
+                is_dicas_block = False
+                break
+            elif is_dicas_block:
+                dicas.append(paragraph)
+        return dicas
+
+    dicas = get_dicas(page)
+    # moves dica to before 'referencias'
+    for paragraph in page:
+        if re.search(REFERENCIAS_HEADER, paragraph.text):
+            for dica in dicas:
+                organized_paragraphs.append(dica)
+        elif paragraph.text not in dicas:
+            organized_paragraphs.append(paragraph)
+    return organized_paragraphs
+
+def format_doc(page_list):
     num_fmt = '{num:02d}. '
     out_pages = []
     for i, page in enumerate(page_list):
-        head = format_header(page)
+        head = generate_header(page)
         filtered_page = field_filter(page)
+        fixed_page = field_reorganize(filtered_page)
+        if not fixed_page:
+            break
         n = num_fmt.format(num=i+1)
-        # 01. (orgao - banca - ano) ← paragraph
-        final_header = [n + head]
+
+        # 01. (orgao - banca - ano) ← start of first paragraph
+        # 'Enunciado' is the rest
+        header_paragraph = Document().add_paragraph(n + head + fixed_page[0].text)
         # out_page = [parag11, parag21, parag31, ...]
-        out_page = final_header + filtered_page
+        out_page = [header_paragraph] + fixed_page[1:]
         # out_pages = [[parag11, parag21, parag31, ...], [parag12, parag22, parag32, ...] ...]
         out_pages.append(out_page)
     return out_pages
 
 def save_doc(path, page_list):
-    d = doc()
-    # first page doesnt need page break
-    for paragraph in page_list[0]:
-        d.add_paragraph(paragraph)
-    for page in page_list[1:]:
-        # TODO set page_breaks?
+    d = Document()
+    for page in page_list:
         for paragraph in page:
-            d.add_paragraph(paragraph)
+            # d.add_paragraph(paragraph)
+            # ugly ass hack
+            d._body._body._insert_p(paragraph._p)
+        # WARNING: documentation doesnt advise overuse of this. Dont know why. Investigate...
+        # It seems like some pages already have this
+        d.add_page_break()
     d.save(path)
 
 if __name__ == '__main__':
-    d = doc(FILE_INPUT_PATH)
-    pages = get_pages(d.paragraphs)
-    right_pages = output_doc(pages)
     paf = FILE_INPUT_PATH
-    save_doc(paf, right_pages)
-    #code.interact(local=locals())
+    d = Document(FILE_INPUT_PATH)
+    pages = get_page_blocks(d.paragraphs)
+    fixed_pages = format_doc(pages)
+    save_doc(paf, fixed_pages)
+
+    if DEBUG:
+        code.interact(local=locals())
