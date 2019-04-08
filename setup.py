999499e0903286316f7d9046ef4c011cabc7e0f1
selflect11 <pedrovolpinacif@gmail.com>
2019-04-07 22:08:13 -0400

    setup file loaded

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from disutils.core import setup
+
+setup(name='Thais_Bot',
+      version='1.0',
+      description='Script que permite Thais ganhar dinheiro apertando um botão.',
+      packages=['python-docx'],
+     )
